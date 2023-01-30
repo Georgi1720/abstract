f43d8cb1303e597d353b1835670b68b8f216f4a8
Georgi <i@gpolkanov.ru>
2023-01-30 16:54:32 +0300

    Less4

---
+++ Lesson4/src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        TaskList taskList = new TaskList();
+        taskList.setTask(new Task(1,"Провести инвентаризацию"));
+        taskList.setTask(new Task(2, "Провести собрание"));
+        taskList.setTask(new Task(3, "Попить кофе"));
+
+        taskList.setTask(new Task(1, "Купить машину"));
+        taskList.setTask(new Task(2, "Заправить машину"));
+        taskList.setTask(new Task(5, "Купить цветы (любовнице)"));
+        System.out.println(taskList.toString());
+        System.out.printf(taskList.getTask());
+        taskList.readUsingFiles();
+    }
+}
